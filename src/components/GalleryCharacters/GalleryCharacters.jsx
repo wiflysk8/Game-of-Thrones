01bdcf7672dac3bd11d76bb0b39df9bdc4b50ac4
Luis Martínez <wifly.sk8@gmail.com>
2022-04-10 20:16:09 +0200

    home scroll fix

--- src/components/GalleryCharacters/GalleryCharacters.jsx
+++ src/components/GalleryCharacters/GalleryCharacters.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import './GalleryCharacters.scss'
-import { Link,generatePath } from 'react-router-dom'
+import React from "react";
+import "./GalleryCharacters.scss";
+import { Link, generatePath } from "react-router-dom";
 
-const GalleryCharacters = ({data}) => {  
-   
+const GalleryCharacters = ({ data }) => {
   return (
     <section className="c-characters">
-    <div className='row'> 
-    {data.map((character) => (
-      <div className='col-12 col-md-6 col-lg-2' key={character.id}>
-      <Link className='c-characters__link' to={generatePath("/characters/:name", {name: character.name})}>  
-      <figure key={character.id} className='c-characters__figure'>
-        <img className="c-characters__figure__img" src={character.image} alt={character.name}/>
-        <p className="c-characters__figure__name">{character.name}</p>
-      </figure> 
-      </Link>
-      </div> 
-      
-    ))}
-    </div>
+      <div className="row">
+        {data.map((character) => (
+          <div className="col-12 col-md-6 col-lg-2" key={character.id}>
+            <Link
+              className="c-characters__link"
+              to={generatePath("/characters/:name", { name: character.name })}
+            >
+              <figure key={character.id} className="c-characters__figure">
+                <img
+                  className="c-characters__figure__img"
+                  src={character.image}
+                  alt={character.name}
+                />
+                <p className="c-characters__figure__name">{character.name}</p>
+              </figure>
+            </Link>
+          </div>
+        ))}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default GalleryCharacters
+export default GalleryCharacters;
