8d22ce938b6d086b3a5127e0fb172edce1d16213
Luis Martínez <wifly.sk8@gmail.com>
2022-04-11 20:39:25 +0200

    change underline active link

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import {NavLink} from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import './Nav.scss'
+import "./Nav.scss";
 
 const Nav = () => {
   const { t } = useTranslation(["translation"]);
   return (
     <div className="c-menu">
-    <nav className="c-menu__nav">
-        <NavLink className="c-menu__link" to="/characters"  style={({ isActive }) => ({ "borderBottom": isActive ? '3px solid white' : 'none' })}>{t("Characters")}</NavLink>
-        <NavLink className="c-menu__link" to="/houses"  style={({ isActive }) => ({ "borderBottom": isActive ? '3px solid white' : 'none' })}>{t("Houses")}</NavLink>
-        <NavLink className="c-menu__link" to="/chrono"  style={({ isActive }) => ({ "borderBottom": isActive ? '3px solid white' : 'none' })}>{t("Chronology")}</NavLink>
-    </nav>
-</div>
-  )
-}
+      <nav className="c-menu__nav">
+        <NavLink className="c-menu__link" to="/characters" style={({ isActive }) => ({ borderBottom: isActive ? "1px solid white" : "none" })}>
+          {t("Characters")}
+        </NavLink>
+        <NavLink className="c-menu__link" to="/houses" style={({ isActive }) => ({ borderBottom: isActive ? "1px solid white" : "none" })}>
+          {t("Houses")}
+        </NavLink>
+        <NavLink className="c-menu__link" to="/chrono" style={({ isActive }) => ({ borderBottom: isActive ? "1px solid white" : "none" })}>
+          {t("Chronology")}
+        </NavLink>
+      </nav>
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
